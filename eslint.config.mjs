efd93cc05cdde351da0a6bef4bd2ef50c9e03d26
Kiminandayo19 <100850217+kiminandayo19@users.noreply.github.com>
2024-11-12 17:06:25 +0700

    fix: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,47 +10,63 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
+export default [
+  {
     ignores: [
-        "**/dist/",
-        "**/coverage",
-        "admin/node_modules",
-        "bin/generators/gateway/templates/basic/server.js",
-        "bin/generators/gateway/templates/getting-started/server.js",
+      "**/eslint.config.mjs",
+      "**/bin/",
+      "**/test/",
+      "**/dist/",
+      "**/coverage",
+      "admin/node_modules",
+      "bin/generators/gateway/templates/basic/server.js",
+      "bin/generators/gateway/templates/getting-started/server.js",
     ],
-}, ...fixupConfigRules(compat.extends(
-    "standard",
-    "plugin:node/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-)), {
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      "standard",
+      "plugin:node/recommended",
+      "plugin:import/errors",
+      "plugin:import/warnings",
+    ),
+  ),
+  {
     plugins: {
-        import: fixupPluginRules(_import),
-        node: fixupPluginRules(node),
+      import: fixupPluginRules(_import),
+      node: fixupPluginRules(node),
     },
 
     languageOptions: {
-        globals: {
-            ...globals.node,
-        },
+      globals: {
+        ...globals.node,
+      },
     },
 
     rules: {
-        "space-before-function-paren": 0,
-        "comma-dangle": [2, "never"],
-        "no-var": "error",
-        semi: [2, "always"],
-        "no-console": "warn",
-        "no-prototype-builtins": "off",
-        "prefer-const": "error",
+      "space-before-function-paren": 0,
+      "comma-dangle": [2, "never"],
+      "no-var": "error",
+      semi: [2, "always"],
+      "no-console": "warn",
+      "no-prototype-builtins": "off",
+      "prefer-const": "error",
 
-        "node/no-deprecated-api": ["error", {
-            ignoreModuleItems: ["crypto.createCipher", "crypto.createDecipher", "url.parse"],
-        }],
+      "node/no-deprecated-api": [
+        "error",
+        {
+          ignoreModuleItems: [
+            "crypto.createCipher",
+            "crypto.createDecipher",
+            "url.parse",
+          ],
+        },
+      ],
     },
-}];
+  },
+];
